be2cd225535906ddc95727ff6f3be7ef426be3c5
roldan-eng-software <roldan.eng.software@gmail.com>
2025-12-12 10:33:31 -0300

    Commit Deploy correto

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/piscineiro-landing/',
+  base: '/LandingPage_Piscineiros/',
   plugins: [react()],
   server: {
     port: 5173,
